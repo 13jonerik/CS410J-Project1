711d54df0e92f49506900b3ace09deeb76068a78
Jon-Erik <jonerik@pdx.edu>
2015-07-07 00:08:41 -0700

    Begin Project1 file to parse the command line

--- src/main/java/edu/pdx/cs410J/jonerik/Project1.java
+++ src/main/java/edu/pdx/cs410J/jonerik/Project1.java
@@ -9,7 +9,16 @@ public class Project1 {
 
   public static void main(String[] args) {
     Class c = AbstractPhoneBill.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    //System.err.println("Missing command line arguments");
+
+    // if command line args aren't good, print this and exit gracefully
+      //System.err.println("Missing command line arguments");
+
+    PhoneCall incoming   = new PhoneCall(args[0], args[1], args[2], args[3], args[4], args[5]);
+    PhoneBill addNewCall = new PhoneBill(args[0], incoming);
+
+    // addNewCall.addPhoneCall(addNewCall)
+    // System.out.println(addNewCall.getPhoneCalls());
+
     for (String arg : args) {
       System.out.println(arg);
     }
